862ac3f7ef129b391da8df383759f539f617efed
Satyam Pal <palsatyam877@gmail.com>
2024-06-08 23:15:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+ReactDOM.createRoot(document.getElementById("root")).render(React.createElement( "div" , {id : "parent"} , 
+   [ 
+    React.createElement("div" , {id : "child"} ,
+      [  React.createElement( "h1" , {} , "Hello React")  
+        , React.createElement( "h1" , {} , "Hello React2")    ]                         
+    ),
+    React.createElement("div" , {id : "child2"} ,
+        [  React.createElement( "h1" , {} , "Hello React")  
+          , React.createElement( "h1" , {} , "Hello React2")    ]                         
+    )
+] ));
